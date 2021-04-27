61fa801a50583e0c7953a3b746b97d0a04ac430b
Michał Zwierzyński <mzwierzynski87@gmail.com>
2021-04-27 12:51:17 +0200

    Draw scene function + background.

--- MetaWarfare/BattleSceneLogic.ino
+++ MetaWarfare/BattleSceneLogic.ino
@@ -1,6 +1,12 @@
+#define GRASSLAND 1
+#define ROAD 2
+#define FOREST 3
+
+int LeftSceneTheme=FOREST;
+int RightSceneTheme=FOREST;
 
 void BattleScene()
 {
-  drawBattleScene(1,6,2,6);
+  drawBattleScene(1,1,2,1);
 }
 
